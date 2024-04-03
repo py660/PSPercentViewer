97448fefff63e1bff62b29eec40146e4848bdef8
py660 <93660666+py660@users.noreply.github.com>
2024-04-02 22:05:18 -0400

    Hush log

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-setInterval(()=>{
+let interval = setInterval(()=>{
 try{
 let quarter = document.querySelector("table.linkDescList").children[0].children[1].children[3].innerText;
 let weights = document.querySelector("table.tablesorter.grid").children[1].children;
@@ -42,5 +42,7 @@ for (let assignment of gradetable.children[1].children){
 }
 
 document.querySelector("table.linkDescList").children[0].children[1].children[4].innerText = document.querySelector("table.linkDescList").children[0].children[1].children[4].innerText.split("\n")[0] + `\n[${~~(totalGrade[0]/totalGrade[1]*1000)/10}/100]`;
+
+clearInterval(interval);
 }catch(e){console.log(e)}
 }, 1000);
