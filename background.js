2c4ceae7ade44fadf859dc0b188bfabcbfa97260
py660 <93660666+py660@users.noreply.github.com>
2024-04-05 17:10:57 -0400

    Update background.js

--- background.js
+++ background.js
@@ -30,10 +30,30 @@ for (let assignment of gradetable.children[1].children){
     }
     //console.log(assignment);
     let category = assignment.children[1].children[1].innerText;
-    let weight = weightsEnabled ? currentWeight[category] : 100;
+
+    let i;
+    if (assignment.children[10].children.length > 1){
+        i = 1;
+    }
+    else{
+        i = 0;
+    }
+    let scoreStr = assignment.children[10].children[i].innerText.split("\n")[0].split("/");
+    //console.log(scoreStr);
+    let weight;
+    if (scoreStr[0].startsWith("(")){
+        console.log("Weighted again");
+        console.log(scoreStr);
+        scoreStr[0] = scoreStr[0].slice(1);
+        scoreStr[1] = scoreStr[1].slice(0,-1);
+        console.log(scoreStr);
+        weight = parseInt(scoreStr[1]);
+    }
+    else{
+        weight = weightsEnabled ? currentWeight[category] : parseInt(scoreStr[1]);
+    }
     //console.log(assignment.children[10].children[0].innerText.split("\n"))
-    let scoreStr = assignment.children[10].children[0].innerText.split("\n")[0].split("/");
-    let score = parseInt(scoreStr[0])/parseInt(scoreStr[1]);
+    let score = parseFloat(scoreStr[0])/parseInt(scoreStr[1]);
     totalGrade[0] += ~~(weight*score*10)/10;
     totalGrade[1] += weight;
     console.log(~~(weight * score *10)/10 + "/" + weight);
